2fb336b648c03a8c84fac81ba6c484b43f9cb128
Roberto Di Remigio <eurobertodr@vglogin0004.vega.izum.si>
2021-09-27 11:32:32 +0200

    Fix CMakeLists.txt

--- content/code/01_hello-sycl/hello.cpp
+++ content/code/01_hello-sycl/hello.cpp
@@ -1,10 +1,12 @@
 #include <CL/sycl.hpp>
 
 int main() {
+  using namespace cl;
+
   auto defaultQueue = sycl::queue{};
 
   defaultQueue
-      .submit([&](sycl::handler& cgh) {
+      .submit([&](sycl::handler &cgh) {
         auto os = sycl::stream{128, 128, cgh};
 
         cgh.single_task<struct hello_world>([=]() { os << "Hello World!\n"; });
