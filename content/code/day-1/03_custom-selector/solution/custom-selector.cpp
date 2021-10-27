45f63b3e5e7c8eaa5932bfefba7662a6fccf3157
Roberto Di Remigio <roberto.diremigio@gmail.com>
2021-10-27 18:27:20 +0200

    Run clang-format

--- content/code/day-1/03_custom-selector/solution/custom-selector.cpp
+++ content/code/day-1/03_custom-selector/solution/custom-selector.cpp
@@ -11,30 +11,29 @@
 
 using namespace sycl;
 
-class custom_selector : public device_selector {
-  public:
-    int operator()(const device &dev) const override {
-      int score = -1;
+class custom_selector : public device_selector
+{
+public:
+  int operator()(const device &dev) const override
+  {
+    int score = -1;
 
-      if (dev.is_gpu()) {
-	auto vendor = dev.get_info<info::device::vendor>();
-        if (vendor.find("AMD")
-            != std::string::npos) score += 75;
+    if (dev.is_gpu()) {
+      auto vendor = dev.get_info<info::device::vendor>();
+      if (vendor.find("AMD") != std::string::npos) score += 75;
 
-        if (vendor.find("NVIDIA")
-            != std::string::npos) score += 50;
+      if (vendor.find("NVIDIA") != std::string::npos) score += 50;
 
-        if (vendor.find("Intel")
-            != std::string::npos) score += 100;
-      }
-
-      // Give host device points so it is used if no GPU is available.
-      // Without these next two lines, systems with no GPU would select
-      // nothing, since we initialize the score to a negative number above.
-      if (dev.is_host()) score += 25;
-
-      return score;
+      if (vendor.find("Intel") != std::string::npos) score += 100;
     }
+
+    // Give host device points so it is used if no GPU is available.
+    // Without these next two lines, systems with no GPU would select
+    // nothing, since we initialize the score to a negative number above.
+    if (dev.is_host()) score += 25;
+
+    return score;
+  }
 };
 
 int
@@ -42,11 +41,11 @@ main()
 {
   std::cout << "Give me any GPU!" << std::endl;
 
-  auto Q = queue{ custom_selector{} };
+  auto Q = queue { custom_selector {} };
 
   std::cout << "we are running on: "
-    << Q.get_device().get_info<info::device::vendor>() << " " 
-    << Q.get_device().get_info<info::device::name>() << std::endl; 
+            << Q.get_device().get_info<info::device::vendor>() << " "
+            << Q.get_device().get_info<info::device::name>() << std::endl;
 
   return 0;
 }
